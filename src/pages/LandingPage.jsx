f4a11f8d7dd152c3a73c6c8b2f418c93a260ef82
Brie Haynes <briehaynes9@gmail.com>
2025-03-27 20:57:34 -0500

    Added functionality for user ID

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,16 +2,41 @@ import '../assets/reset.css';
 import CSS from './LandingPage.module.css';
 import airplane from '../assets/images/airplane.png';
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react';
+import { axiosInstance } from '../../client';
 
 const LandingPage = () => {
+  const [userId, setUserId] = useState(null);
   const nav = useNavigate();
+
+  const handleClick = async () => {
+    let storedId = localStorage.getItem("user_id");
+
+    if (!storedId) {
+      storedId = crypto.randomUUID();
+      localStorage.setItem("user_id", storedId);
+    }
+
+    setUserId(storedId);
+
+    try {
+      const res = await axiosInstance.post(`/add-user/`, {
+        user_id: storedId
+      });
+      console.log("Response: ", res.data);
+      nav('/home');
+    } catch (error) {
+      alert("Unable to create user ID. Please try again.");
+      console.log(error.response.data);
+    }
+  }
 
   return (
     <div id={CSS.landingPage}>
     <div className={CSS.container}>
         <div id={CSS.navbar}>
             <p>LOGO</p>
-            <button onClick={() => nav('/home')} className={CSS.button}>Get started</button>
+            <button onClick={handleClick} className={CSS.button}>Get started</button>
         </div>
 
         <div id={CSS.mainContent}>
