d29acb35c2b1e2f4c6fcb352d15676164c3ae7fc
Brie Haynes <briehaynes9@gmail.com>
2025-03-30 13:17:36 -0500

    Added state management and client

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,19 @@ import '../assets/styles.css';
 import '../assets/reset.css'
 import Sidebar from '../components/Sidebar/Sidebar';
 import MainDisplay from '../components/MainDisplay/MainDisplay';
+import { useEffect } from 'react';
+import useUserStore from '../store/UserStore';
+import { handleGetUserData } from '../handlers/user-handler';
 
 const HomePage = () => {
+    const { userId } = useUserStore();
+
+    useEffect(() => {
+        if (userId) {
+            handleGetUserData(userId);
+        }
+    }, [userId]);
+
     return (
         <div className="container">
             <Sidebar />
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,27 +2,57 @@ import '../assets/reset.css';
 import CSS from './LandingPage.module.css';
 import airplane from '../assets/images/airplane.png';
 import { useNavigate } from 'react-router-dom'
+import useUserStore from '../store/UserStore';
+import { handleCreateUser, handleGetUser } from '../handlers/user-handler';
+import { useEffect } from 'react';
 
 const LandingPage = () => {
   const nav = useNavigate();
+  const { 
+    userId, 
+    user, 
+    loading, 
+    navigate, 
+    error
+  } = useUserStore();
+
+  const handleClick = async () => {
+    if (!userId) {
+      await handleCreateUser();
+    } else {
+      await handleGetUser(userId);
+    }
+  }
+
+  useEffect(() => {
+    if (user && !loading && navigate) {
+      nav(`/home`);
+    }
+  }, [loading, user, navigate, nav])
+
+  useEffect(() => {
+    if (error) {
+      alert("Unable to verify/create user. Please try again.")
+    }
+  }, [error])
 
   return (
     <div id={CSS.landingPage}>
-    <div className={CSS.container}>
-        <div id={CSS.navbar}>
-            <p>LOGO</p>
-            <button onClick={() => nav('/home')} className={CSS.button}>Get started</button>
-        </div>
-
-        <div id={CSS.mainContent}>
-            <h1 id={CSS.heading}>Predict CO and NOx Emissions</h1>
-            <div id={CSS.tagline}>
-                <p className={CSS.text}>Generate custom emission predictions and</p>
-                <p className={CSS.text}>visualizations based on engine parameter data</p>
-            </div>
-            <img id={CSS.airplane} src={airplane} />
-        </div>
-    </div>
+      <div className={CSS.container}>
+          <div id={CSS.navbar}>
+              <p>LOGO</p>
+              <button onClick={handleClick} className={CSS.button}>Get started</button>
+          </div>
+  
+          <div id={CSS.mainContent}>
+              <h1 id={CSS.heading}>Predict CO and NOx Emissions</h1>
+              <div id={CSS.tagline}>
+                  <p className={CSS.text}>Generate custom emission predictions and</p>
+                  <p className={CSS.text}>visualizations based on engine parameter data</p>
+              </div>
+              <img id={CSS.airplane} src={airplane} />
+          </div>
+      </div>
     </div>
   );
 };
